7681dfb090680e7842e296f7e151b3ddd0536c52
DouglasKugler <douglaskugler99@gmail.com>
2024-10-09 18:42:19 -0300

    .

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
-from .forms import OrdemServicoForm
+from .forms import OrdemServicoForm, UserRegistrationForm
 from .models import EmissaoOrdemServico
 from openpyxl import Workbook
 from django.http import HttpResponse
